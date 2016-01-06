07f530d5ebc1fa75f54a7d3ff593a2d333bcc9e7
Jeffrey Leonard <jeffrey9279@gmail.com>
2016-01-06 11:21:19 -0800

    works

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 var express = require('express'),
     mongoose = require('mongoose'),
     shortUrl = require('./shortUrl'),
-    bodyParser = require('body-parser')
+    bodyParser = require('body-parser'),
+    autoIncrement = require('mongoose-auto-increment')
 
 var app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.json({type: 'application/vnd.api+json'}))
 
-mongoose.connect('mongodb://localhost:27017/urls')
+var connection = mongoose.connect('mongodb://localhost:27017/urls')
 
 app.get('/new/:url', function(req, res){
-  var newDoc = new shortUrl({ 'fullUrl': "weeee" });
+  var newDoc = new shortUrl({ 'fullUrl': req.params.url });
                    newDoc.save(function (err, doc) {
                        if (err) { throw err; }
 
@@ -22,11 +23,17 @@ app.get('/', function(req, res){
      res.send('enter a url')
 } )
 
-app.get('/go/:shortUrl' function(req, res){
-  shortUrl.findOne({ _id: req.params.shortUrl } function(err, shortUrl){
+app.get('/go/:shortUrl', function(req, res){
+  shortUrl.findOne({ _id: req.params.shortUrl }, function(err, shortUrl){
       if (err) { throw err; }
-      res.redirect('http://google.com')
+      if shortUrl{
+          res.redirect("http://" + shortUrl.fullUrl)
+      }else{
+        res.redirect('/')
+      }
+
   })
+
 })
 
 app.listen(8080)
--- shortUrl.js
+++ shortUrl.js
@@ -1,9 +1,13 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var autoIncrement = require('mongoose-auto-increment')
+
+var connection = mongoose.createConnection('mongodb://localhost:27017/urls')
+autoIncrement.initialize(connection)
 
 var ShortUrl = new Schema(
   { fullUrl: String }
 )
 
-
+ShortUrl.plugin(autoIncrement.plugin, 'ShortUrl')
 module.exports = mongoose.model('ShortUrl', ShortUrl);
